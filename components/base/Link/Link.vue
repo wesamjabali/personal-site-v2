d2193a226a92af9e3fce8654815517e4a7b3e560
Wesam Jabali <wesamjabali@icloud.com>
2022-07-24 19:12:25 -0500

    🆕 implement baseLinks

---
+++ components/base/Link/Link.vue
@@ -0,0 +1,40 @@
+<template>
+    <a :href="href" :class="`base-link base-link__${props.type ?? 'inline'}`">
+        <slot />
+    </a>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{ href: string, type: "navigation" | "inline" }>()
+</script>
+
+<style lang="scss">
+.base-link {
+    color: $primary;
+
+    &:visited {
+        color: $primary
+    }
+
+    &__navigation {
+        padding: 1rem;
+        text-decoration: none;
+        background-color: rgba(255, 255, 255, 0.1);
+        border-radius: 0.5rem;
+        margin-top: 1rem;
+        box-sizing: border-box;
+        width: 100%;
+        text-align: center;
+
+        &:hover {
+            background-color: white;
+            color: black;
+        }
+
+        &::before {
+            content: '#️';
+        }
+    }
+
+}
+</style>
